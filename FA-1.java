be5c50d188a2c7d4421e20f55692d462b9aff268
Sumair <mailstosumair@gmail.com>
2023-09-28 13:51:22 +0530

    new file:   FA-1.java 	new file:   Sum_Of_Even_And_Odd.java

---
+++ FA-1.java
@@ -0,0 +1,5 @@
+public class FA-1 {
+    public static void main(String args[]){
+        
+    }
+}
---
+++ Sum_Of_Even_And_Odd.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Sum_Of_Even_And_Odd {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        //Input
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        //Logic
+        int Even_Sum = 0;
+        int Odd_Sum = 0;
+
+        while(num != 0){
+            int digit = num%10;
+            if(digit%2 == 0){
+                Even_Sum = Even_Sum + digit;
+            }
+            else if(num % 2 != 0){
+                Odd_Sum = Odd_Sum + digit;
+            }
+            num = num/10;
+        }
+        System.out.print("The sum of even digits is: "+Even_Sum);
+        System.out.print("The sum of odd digits is: "+Odd_Sum);
+    }
+}
