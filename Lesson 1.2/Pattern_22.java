143078b9e1d14e99d13af3beabc5fd5a2aac4a93
Sumair <mailstosumair@gmail.com>
2023-11-04 15:16:35 +0530

    new file:   Lesson 1.2/Pattern_22.java

---
+++ Lesson 1.2/Pattern_22.java
@@ -0,0 +1,20 @@
+// Sample Input 1:
+// 3
+// Sample Output 1:
+// 33333
+// 32223
+// 32123
+// 33333
+import java.util.Scanner;
+public class Pattern_22{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        //Input
+        int Input = sc.nextInt();
+        //Logic
+        for(int i=0;i<=Input;i++){
+            
+        }
+        sc.close();
+    }
+}
